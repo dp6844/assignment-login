e3994cfca1c65f5eeeaa42441d4c3a4a06f9bfd6
ddpatel <dp827448@dal.ca>
2020-06-07 15:23:44 -0300

    Bug fixed

--- login.js
+++ login.js
@@ -1,28 +1,14 @@
+require('dotenv').config()
+
 const express = require('express');
-const session = require('express-session')
 const mysql = require('mysql2');
+const jwt = require('jsonwebtoken');
 const app = express();
+let cors = require('cors')
+
+app.use(cors())
 app.use(express.json());
 
-const two_hours = 1000*60*60*2
-
-const {
-    SESS_NAME = 'sid',
-    SESS_SECRET = 'secret',
-    SESS_LIFETIME = two_hours,
-} = process.env
-
-app.use(session({
-    name: SESS_NAME,
-    resave: false,
-    saveUninitialized: false,
-    secret: SESS_SECRET,
-    cookie:{
-        maxAge: SESS_LIFETIME,
-        sameSite: true,
-        secure: false
-    }
-}))
 
 const bodyParser = require('body-parser');
 
@@ -46,87 +32,98 @@ db.connect((err) => {
 const jsonParser = bodyParser.json()
 
 app.post('/api/login', jsonParser, (req, res) => {
-    console.log(req.session);
-    if (!req.session.userID){
-        let email = req.body.email;
-        let password = req.body.password;
-        let sqlSelect = 'SELECT * FROM user_details WHERE email ="' + email + '";';
-        let sqlSelect1 = 'SELECT * FROM user_details WHERE password ="' + password + '";';
-        let querySelect = db.query(sqlSelect, (err, result) => {
-            console.log(result);
-            if (result != "") {
-                let queryselect1 =db.query(sqlSelect1,(err, result1) =>{
-                    if(result1 != ""){
-                        console.log(result1);
-                        let state = "online";
-                        let today = new Date();
-                        let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-                        let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-                        let dateTime = date+' '+time;
-                        let sqlSelect2 = 'SELECT * FROM user_state WHERE email ="' +  email + '";';
-                        let sql = 'INSERT INTO user_state values( "' + email + '", "' + state + '", "' + dateTime +'", " ");';
-                        let sql1 = 'UPDATE user_state SET state ="'+ state +'", login_time = "'+ dateTime +'", logout_time = " " WHERE email = "'+ email +'";';
-                        
-                        let querySelect = db.query(sqlSelect2, (err, result) => {
-                            console.log(result);
-                            if (result == "") {
-                                let query = db.query(sql, (err, userstate) => {
-                                if (err) {
-                                    throw err;
-                                }
-                            console.log(`User state (${email}, ${dateTime}) inserted in the table`);
-                            });
-                            }else{
-                                let query = db.query(sql1, (err, userstate) => {
-                                    if (err) {
-                                        throw err;
-                                    }
-                                console.log(`User state (${email}, ${state}, ${dateTime}) inserted in the table`);
-                                });
+    console.log({ req })
+    let email = req.body.email;
+    let password = req.body.password;
+    let sqlSelect = 'SELECT * FROM user_details WHERE email ="' + email + '";';
+    let sqlSelect1 = 'SELECT * FROM user_details WHERE email ="' + email + '" and password ="' + password + '";';
+    let querySelect = db.query(sqlSelect, (err, result) => {
+        if (result != "") {
+            let queryselect1 = db.query(sqlSelect1, (err, result1) => {
+                if (result1 != "") {
+                    const user = { email }
+                    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+                    let state = "online";
+                    let today = new Date();
+                    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
+                    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+                    let dateTime = date + ' ' + time;
+                    let sqlSelect2 = 'SELECT * FROM user_state WHERE email ="' + email + '";';
+                    let sql = 'INSERT INTO user_state values( "' + email + '", "' + state + '", "' + dateTime + '", " ","' + accessToken + '");';
+                    let sql1 = 'UPDATE user_state SET state ="' + state + '", login_time = "' + dateTime + '", logout_time = " ", access_token = "' + accessToken + '" WHERE email = "' + email + '";';
+
+                    let querySelect =  db.query(sqlSelect2, async (err, result) => {
+                        console.log(result);
+                        if (result == "") {
+                            try{
+                                let query = await db.query(sql);
+                                console.log(`User state (${email}, ${dateTime}) inserted in the table`);
+                            }catch(err){
+                                console.log(err)
                             }
-                        });
-                        req.session.userID = email;
-                        res.send(`Hi,welcome ${result[0].name} you are logged-in.`);
-                    }else{
-                        res.send(`Password for user ${email} is incorrect`);
-                    }    
-                });
-            } else {
-                console.log('You are new user register before login');
-                res.status(404).send('You are new user register before login');
-            }
-        });
-    }else{
-        res.send("Your already logged-in")
-    }
+                        } else {
+                            try{
+                                let query = await db.query(sql1);
+                                console.log(`User state (${email}, ${state}, ${dateTime}) inserted in the table`); 
+                            }catch(err){
+                                console.log(err);
+                            }
+                        }                        
+                    });
+                    res.json({ user: result[0].name, accessToken: accessToken })
+                } else {
+                    res.send(`Password for user ${email} is incorrect`);
+                }
+            });
+        } else {
+            console.log('You are new user register before login');
+            res.status(404).send('You are new user register before login');
+        }
+    });
 });
 
-app.post('/api/logout', jsonParser, (req, res) => {
-    console.log(req.session)
-    if(req.session.userID){
-    let email = req.body.email;
+function authenticateToken(req, res, next) {
+    const token = req.headers['token']
+    if (token == null) return res.status(401).send("You are Unauthorized user")
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res.status(403).send("You are Unauthorized user")
+        req.user = user;
+        next();
+    })
+}
+
+app.get('/api/logout', jsonParser, authenticateToken, (req, res) => {
+    let email = req.user.email;
+    let access_token = req.headers.token;
     let state = "offline";
     let today = new Date();
-    let date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
+    let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
     let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-    let dateTime = date+' '+time;
-    let sql = 'UPDATE user_state SET state ="'+ state +'", logout_time = "'+ dateTime +'" WHERE email = "'+ email +'";';
-    let query = db.query(sql, (err, userstate) => {
-        if (err) {
-            throw err;
-         }
-    console.log(`User logout successful (${email}, ${state}, ${dateTime}) inserted in the table`);
-    req.session.destroy(err =>{
-        if(err){
-            throw err;
+    let dateTime = date + ' ' + time;
+    let sqlSelect = 'SELECT access_token FROM user_state WHERE email ="' + email + '";';
+    let sql = 'UPDATE user_state SET state ="' + state + '", logout_time = "' + dateTime + '", access_token = " " WHERE email = "' + email + '";';
+    let querySelect = db.query(sqlSelect, (err, result) => {
+        let token = result[0].access_token;
+        if (token) {
+            // if (token === access_token) {
+                let query = db.query(sql, (err, userstate) => {
+                    if (err) {
+                        throw err;
+                    }
+                    console.log(`User logout successful (${email}, ${state}, ${dateTime}) inserted in the table`);
+                    res.send("logout successful");
+                });
+            // } else {
+            //     console.log('Login First');
+            //     res.status(404).send("Login First");
+            // }
+        } else {
+            console.log('Login First');
+            res.status(404).send('Login First');
         }
-        res.clearCookie(SESS_NAME).send("logout successful");
-    })
     });
-}else{
-    res.send("login first");
-}
+
 });
 
-const port = process.env.PORT || 5000;
-app.listen(port, () => console.log('listening on port....' + port));
+const port = process.env.PORT || 3001;
+app.listen(port, () => console.log('listening on port....' + 3001));
